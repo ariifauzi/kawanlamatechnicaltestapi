f7373835364febadb561e2625c5fc512e03f1b5f
user <ariiifauzi23031997@gmail.com>
2025-05-06 09:46:55 +0700

    fix tc

--- cypress/e2e/APITesting/HTTPRequests.cy.js
+++ cypress/e2e/APITesting/HTTPRequests.cy.js
@@ -1,5 +1,50 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("HTTP Requests", ()=> {
+
+    it("POST User", ()=> {
+        cy.request({
+                method: 'POST',
+                url: 'https://petstore.swagger.io/v2/user',
+                body: {
+                        id: 1,
+                        username: "Aser",
+                        firstName: "Ashari",
+                        lastName: "Fauzi",
+                        email: "test@gmail.com",
+                        password: "12345678",
+                        phone: "08123456789",
+                        userStatus: 0
+                }
+        })
+
+        .its('status')
+        .should('equal', 200);
+    })
+
+
+    it("GET User", ()=>{
+      cy.request('GET', 'https://petstore.swagger.io/v2/user/Aser')
+      .its('status')
+      .should('equal', 200);
   })
+
+    it("PUT User", ()=>{
+        cy.request({
+            method: 'PUT',
+            url: 'https://petstore.swagger.io/v2/user/Aser',
+            body: {
+                    id: 1,
+                    username: "Aser",
+                    firstName: "Ashari",
+                    lastName: "Fauzi",
+                    email: "test@gmail.com",
+                    password: "123456789",
+                    phone: "08123456789",
+                    userStatus: 0
+            }
+        })
+        
+        .its('status')
+        .should('equal', 200);
+    })
+
 })
